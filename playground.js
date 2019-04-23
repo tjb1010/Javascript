67e7eb5e6f433872eb43b7e85918435b4f83e532
tjb1010 <bickford1010@gmail.com>
2019-04-23 15:34:59 -0500

    add git ignore

--- playground.js
+++ playground.js
@@ -1,4 +1,13 @@
-x = 5;
-y = 10;
+function Car(year, make, model) {
+  this.year = year;
+  this.make = make;
+  this.model = model;
+  this.numWheels = 4;
+}
 
-console.log(x + y);
+Car.prototype.log = function() {
+  console.log(this);
+};
+var myCar = new Car(2012, "Chevy", "Malibu");
+
+myCar.log();
