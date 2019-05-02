c19791db8bd37557925e684cdd1f9bdb9c61dfac
tjb1010 <bickford1010@gmail.com>
2019-05-02 09:17:44 -0500

    added fetch api to get robot list

--- react/robofriends/src/App.js
+++ react/robofriends/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
-import robots from './robots';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      robots: [],
       searchfield: '',
     };
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   onSearch = (event) => {
@@ -16,10 +22,14 @@ class App extends Component {
   };
 
   render() {
-    const { searchfield } = this.state;
+    const { robots, searchfield } = this.state;
     const filteredRobots = robots.filter((robot) => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
+
+    if (robots.length === 0) {
+      return <h1>Loading...</h1>;
+    }
     return (
       <div className="tc">
         <h1 className="f1">Robofriends</h1>
