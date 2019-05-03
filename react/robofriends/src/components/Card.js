74527a50dca2045e303ed52152bddcc55bf77c41
tjb1010 <bickford1010@gmail.com>
2019-05-03 10:45:19 -0500

    made changes for performance/pwa

--- react/robofriends/src/components/Card.js
+++ react/robofriends/src/components/Card.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export default function Card({ name, email, id }) {
   return (
     <div className="bg-light-green tc dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
+      <img src={`https://robohash.org/${id}?size=200x200`} alt="robots" />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
