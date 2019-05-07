1aa42d89d722db41178ad9a0d62b3ec91310926a
tjb1010 <bickford1010@gmail.com>
2019-05-07 09:37:16 -0500

    added unit tests for actions/reducers

--- react/robofriends/src/actions.js
+++ react/robofriends/src/actions.js
@@ -4,6 +4,7 @@ import {
   REQUEST_ROBOTS_SUCCESS,
   REQUEST_ROBOTS_FAILED,
 } from './constants';
+import { apiCall } from './api/api';
 
 export const setSearchField = text => ({
   type: CHANGE_SEARCH_FIELD,
@@ -12,8 +13,7 @@ export const setSearchField = text => ({
 
 export const requestRobots = () => (dispatch) => {
   dispatch({ type: REQUEST_ROBOTS_PENDING });
-  fetch('http://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
+  apiCall('http://jsonplaceholder.typicode.com/users')
     .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
     .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }));
 };
