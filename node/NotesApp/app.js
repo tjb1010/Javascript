5f0a4439a119c76c0cefbd7a0d3ec50a9cf0b6f9
tjb1010 <bickford1010@gmail.com>
2019-06-14 16:01:57 -0500

    Add note/remove note functionality

--- node/NotesApp/app.js
+++ node/NotesApp/app.js
@@ -1,7 +1,60 @@
 const chalk = require('chalk');
-const getNotes = require('./notes');
+const yargs = require('yargs');
+const notes = require('./notes');
 
-const msg = getNotes();
-console.log(msg);
+// yargs add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    notes.add(argv.title, argv.body);
+  }
+});
 
-console.log(chalk.blue.inverse.bold('Success!'));
+// yargs remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  builder: {
+    title: {
+      describe: 'Note title to delete',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    notes.remove(argv.title);
+  }
+});
+
+// yargs list command
+yargs.command({
+  command: 'list',
+  describe: 'List all notes',
+  handler: function(argv) {
+    console.log('List of notes!', argv);
+  }
+});
+
+// yargs read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function(argv) {
+    console.log('Reading the note!', argv);
+  }
+});
+
+yargs.parse();
--- node/NotesApp/notes.js
+++ node/NotesApp/notes.js
@@ -1,5 +1,59 @@
-const getNotes = function() {
+const fs = require('fs');
+const chalk = require('chalk');
+
+const listNotes = function() {
   return 'Your notes...';
 };
 
-module.exports = getNotes;
+const addNote = function(title, body) {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(function(note) {
+    return note.title === title;
+  });
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    });
+    saveNotes(notes);
+    console.log(chalk.green.inverse.bold(`Note "${title}" added!`));
+  } else {
+    console.log(chalk.red.inverse.bold(`Note "${title}" already exists!`));
+  }
+};
+
+const removeNote = function(title) {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter(function(note) {
+    return note.title !== title;
+  });
+
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse.bold(`The note "${title}" removed!`));
+    saveNotes(notesToKeep);
+  } else {
+    console.log(chalk.red.inverse.bold(`The note "${title}" does not exist!`));
+  }
+};
+
+const saveNotes = function(notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+};
+
+const loadNotes = function() {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (err) {
+    return [];
+  }
+};
+
+module.exports = {
+  list: listNotes,
+  add: addNote,
+  remove: removeNote
+};
