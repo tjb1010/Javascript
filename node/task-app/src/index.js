a7677f49b136e2367b17cd1f7f39d3a7fbc754ed
tjb1010 <bickford1010@gmail.com>
2019-06-28 08:50:53 -0500

    add bcrypt for password hashing

--- node/task-app/src/index.js
+++ node/task-app/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-require('./db/mongoose');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
+require('./db/mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
